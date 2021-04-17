edd56188c288908f0be47dd4425d06bb197c8023
Andrew MacLeod <andrewmacleod.com@gmail.com>
2021-04-17 10:58:17 +0100

    Added initial failing unit test for Order Processor service.

---
+++ BusinessRulesEngine.Services.UnitTests/GivenAnOrderProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BusinessRulesEngine.Services.UnitTests
+{
+    [TestFixture]
+    public class GivenAnOrderProcessor
+    {
+        private OrderProcessor _orderProcessor;
+
+        [SetUp]
+        public void Setup()
+        {
+            _orderProcessor = new OrderProcessor();
+        }
+
+        [Test]
+        public void WhenProcessOrdersIsInvokedWithNullOrders_ThenAnArgumentNullExceptionIsThrown()
+        {
+            Action act = () => _orderProcessor.ProcessOrders(null);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        //This needs to take in orders, get any rules that apply and then apply them.
+    }
+
+    public class OrderProcessor
+    {
+        public void ProcessOrders(object o)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
