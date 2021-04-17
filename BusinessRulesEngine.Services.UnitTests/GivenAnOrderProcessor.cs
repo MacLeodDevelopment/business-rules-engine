1d640a07066386750d28f47c818749e78dd84e57
Andrew MacLeod <andrewmacleod.com@gmail.com>
2021-04-17 11:18:27 +0100

    Added another failing test to check that rules returned from the match are passed to the rules engine along with the order.

--- BusinessRulesEngine.Services.UnitTests/GivenAnOrderProcessor.cs
+++ BusinessRulesEngine.Services.UnitTests/GivenAnOrderProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
@@ -10,13 +11,20 @@ namespace BusinessRulesEngine.Services.UnitTests
     {
         private OrderProcessor _orderProcessor;
         private Order _validOrder;
+        private IEnumerable<IRule> _expectedMatchingRules;
         private Mock<IRuleMatchService> _mockRuleMatchService;
+        private Mock<IRuleEngine> _mockRuleEngine;
 
         [SetUp]
         public void Setup()
         {
             _validOrder = new Order();
+
+            _expectedMatchingRules = new List<IRule>();
             _mockRuleMatchService = new Mock<IRuleMatchService>();
+            _mockRuleMatchService.Setup(m => m.GetMatchingRules(_validOrder)).Returns(_expectedMatchingRules);
+            
+            _mockRuleEngine = new Mock<IRuleEngine>();
 
             _orderProcessor = new OrderProcessor(_mockRuleMatchService.Object);
         }
@@ -39,12 +47,29 @@ namespace BusinessRulesEngine.Services.UnitTests
             _mockRuleMatchService.Verify(m => m.GetMatchingRules(_validOrder), Times.Once());
         }
 
+        [Test]
+        public void AndThereAreSomeMatchingRules_ThenApplyRulesIsInvokedOnTheRuleEngineWithTheMatchingRulesAndTheOrder()
+        {
+            _orderProcessor.ProcessOrder(_validOrder);
+
+            _mockRuleEngine.Verify(m => m.ApplyRules(_expectedMatchingRules, _validOrder), Times.Once());
+        }
+
         //This needs to take in orders, get any rules that apply and then apply them.
+    }
+
+    public interface IRuleEngine
+    {
+        void ApplyRules(IEnumerable<IRule> rules, Order validOrder);
     }
 
     public interface IRuleMatchService
     {
-        void GetMatchingRules(Order order);
+        IEnumerable<IRule> GetMatchingRules(Order order);
+    }
+
+    public interface IRule
+    {
     }
 
     public class OrderProcessor
@@ -63,7 +88,7 @@ namespace BusinessRulesEngine.Services.UnitTests
                 throw new ArgumentNullException(nameof(order));
             }
 
-            _ruleMatchService.GetMatchingRules(order);
+            var rules = _ruleMatchService.GetMatchingRules(order);
         }
     }
 
