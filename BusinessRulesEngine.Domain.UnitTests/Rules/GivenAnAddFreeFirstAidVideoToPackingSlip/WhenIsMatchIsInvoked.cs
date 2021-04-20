836c3724fa33ab69337ab892eb29a1d1ab1d433f
Andrew MacLeod <andrewmacleod.com@gmail.com>
2021-04-20 14:26:11 +0100

    Renamed ambiguous unit test and prevented risk of false positive pass.

--- BusinessRulesEngine.Domain.UnitTests/Rules/GivenAnAddFreeFirstAidVideoToPackingSlip/WhenIsMatchIsInvoked.cs
+++ BusinessRulesEngine.Domain.UnitTests/Rules/GivenAnAddFreeFirstAidVideoToPackingSlip/WhenIsMatchIsInvoked.cs
@@ -13,20 +13,24 @@ namespace BusinessRulesEngine.Domain.UnitTests.Rules.GivenAnAddFreeFirstAidVideo
     {
         private AddFreeFirstAidVideoToPackingSlip _addFreeFirstAidVideoToPackingSlip;
         private Order _orderWithLearnToSkiVideo;
+        private Product _learningToSkiVideo; 
         private Order _orderWithAnythingElse;
-        private Mock<Order> _orderBefore1997;
+        private Mock<Order> _orderBefore1998;
 
         [SetUp]
         public void Setup()
         {
+            _learningToSkiVideo = new Product(new ProductConfig {SubType = "Video", Name = "Learning to Ski"});
+
             _orderWithLearnToSkiVideo = new Order(new OrderConfig { Id = "Learning to Ski Video Order" });
-            _orderWithLearnToSkiVideo.SetProduct(new Product(new ProductConfig { SubType = "Video", Name  = "Learning to Ski"}));
+            _orderWithLearnToSkiVideo.SetProduct(_learningToSkiVideo);
 
             _orderWithAnythingElse = new Order(new OrderConfig { Id = "Other Order" });
             _orderWithAnythingElse.SetProduct(new Product(new ProductConfig { SubType = "Other Product" }));
 
-            _orderBefore1997 = new Mock<Order>();
-            _orderBefore1997.SetupGet(m => m.Timestamp).Returns(new DateTimeOffset(1997, 12, 31, 23, 59, 59, TimeSpan.Zero));
+            _orderBefore1998 = new Mock<Order>();
+            _orderBefore1998.SetupGet(m => m.Product).Returns(_learningToSkiVideo);
+            _orderBefore1998.SetupGet(m => m.Timestamp).Returns(new DateTimeOffset(1997, 12, 31, 23, 59, 59, TimeSpan.Zero));
             
             _addFreeFirstAidVideoToPackingSlip = new AddFreeFirstAidVideoToPackingSlip(new Mock<IServiceBus>().Object);
         }
@@ -47,9 +51,9 @@ namespace BusinessRulesEngine.Domain.UnitTests.Rules.GivenAnAddFreeFirstAidVideo
         }
 
         [Test]
-        public void AndTheOrderDateIsBefore1997_ThenFalseIsReturned()
+        public void AndTheOrderYearIsBefore1997OrEqualTo1997_ThenFalseIsReturned()
         {
-            var actual = _addFreeFirstAidVideoToPackingSlip.IsMatch(_orderBefore1997.Object);
+            var actual = _addFreeFirstAidVideoToPackingSlip.IsMatch(_orderBefore1998.Object);
             actual.Should().BeFalse();
         }
     }
